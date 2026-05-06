50db42ccf6e92587c3646936337600dee160bc8a
bi tia <tiajoce7@gmail.com>
2026-05-06 11:11:15 +0000

    Teste que arrange() ne modifie pas l'ordre d'insertion retourné par books()

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,6 +1,7 @@
 package bookstoread;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BookShelf {
     private final List<String> books = new ArrayList<>();
@@ -15,7 +16,6 @@ public class BookShelf {
     }
 
     public List<String> arrange() {
-        books.sort(Comparator.naturalOrder());
-        return books;
+        return books.stream().sorted().collect(Collectors.toList());
     }
 }
