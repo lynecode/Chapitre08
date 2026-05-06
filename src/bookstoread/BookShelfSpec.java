028ef18678bf3be3cf80b9ae6f95fc64a0ed19f1
bi tia <tiajoce7@gmail.com>
2026-05-06 10:54:00 +0000

    Ajoute la méthode init() avec @BeforeEach pour réinitialiser l'étagère avant chaque test

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,12 +1,21 @@
 package bookstoread;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class BookShelfSpec {
+    private BookShelf shelf;
+
+    @BeforeEach
+    void init() throws Exception {
+        shelf = new BookShelf();
+    }
+
     @Test
     public void shelfEmptyWhenNoBookAdded() throws Exception {
         BookShelf shelf = new BookShelf();
